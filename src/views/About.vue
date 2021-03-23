d7f2bd45a65d45f520072ff780d087a1240f8510
youhui1214 <405852890@qq.com>
2021-03-23 21:02:35 +0800

    feat: 输入框outline

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,68 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <div class="change">
+            <div class="loginMain">
+
+                <div class="login">密码登录</div>
+                <div class="lineT"></div>
+            </div>
+
+            <div class="Main">
+
+                <div class="login">短信登录</div>
+                <div class="lineT"></div>
+            </div>
+
+        </div>
+    </div>
 </template>
+
+<style lang="stylus" scoped>
+.about
+    //background-color aqua
+    .change
+        //display inline-block
+        .loginMain
+            display: inline-block
+            //background-color antiquewhite
+            width: 80px;
+            //height 100%
+            .login
+                width: 80px;
+                height: 18px;
+                font-size: 20px;
+                font-family: Microsoft YaHei;
+                font-weight: 400;
+                color: #333333;
+
+            .lineT
+                margin-top 10px
+                width: 80px;
+                height: 2px;
+                background: #5076F4;
+                border-radius: 1px;
+
+        .Main
+            display inline-block
+            margin-left 20px
+
+            //background-color #282726
+            //width: 80px;
+            //height 100%
+
+            .login
+                width: 80px;
+                height: 18px;
+                font-size: 20px;
+                font-family: Microsoft YaHei;
+                font-weight: 400;
+                color: #333333;
+
+            .lineT
+                margin-top 10px
+                width: 80px;
+                height: 2px;
+                background: #5076F4;
+                border-radius: 1px;
+
+</style>
---
+++ src/views/Select.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="select">
+        <input type="text">
+    </div>
+</template>
+
+<script>
+
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+    name: 'Select',
+    components: {
+        HelloWorld
+    }
+}
+</script>
+
+<style scoped lang="stylus">
+
+.select
+    margin: 50px;
+
+    input
+        width: 480px;
+        height: 40px;
+        background: #FFFFFF;
+        border: 1px solid #E9E9E9;
+        border-radius: 4px;
+
+        &:focus {
+            outline: 0;
+            border: 1px solid #5076F4;
+            box-shadow: 0 0 10px 0 #5076F4;
+        }
+
+
+
+</style>
