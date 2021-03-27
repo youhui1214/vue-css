25a96f234bfcd15bfde1f6f63c78dabbf1bb011e
youhui <405852890@qq.com>
2021-03-27 19:58:16 +0800

    feat: 切换登录框验证

--- src/views/Select.vue
+++ src/views/Select.vue
@@ -1,22 +1,44 @@
 <template>
     <div class="select">
-        <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-            <el-form-item label="用户名" prop="name">
-                <el-input v-model.number="ruleForm.name">
-                    <i slot="prefix" class="account"></i>
-                </el-input>
-            </el-form-item>
-            <el-form-item label="密码" prop="pass">
-                <el-input type="password" v-model="ruleForm.pass" autocomplete="off">
-                    <i slot="prefix" class="el-input__icon el-icon-search"></i>
-                </el-input>
-            </el-form-item>
+        <el-button @click="tabFrom" type="primary" style="margin:20px;">主要按钮</el-button>
+        <div v-show="isShow" class="one">
+            <el-form ref="ruleForm" :model="ruleForm" status-icon :rules="rules" label-width="100px" class="demo-ruleForm">
+                <el-form-item label="用户名" prop="name">
+                    <el-input v-model="ruleForm.name">
+                        <i slot="prefix" class="account"></i>
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="pass">
+                    <el-input type="password" v-model="ruleForm.pass">
+                        <i slot="prefix" class="el-input__icon el-icon-search"></i>
+                    </el-input>
+                </el-form-item>
 
-            <el-form-item>
-                <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-                <el-button @click="resetForm('ruleForm')">重置</el-button>
-            </el-form-item>
-        </el-form>
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+                    <el-button @click="resetForm('ruleForm')">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+        <div v-show="!isShow" class="two">
+            <el-form ref="rulePhone" :model="rulePhone" status-icon :rules="rulesPhone" label-width="100px" class="demo-ruleForm">
+                <el-form-item label="手机号" prop="phone">
+                    <el-input v-model="rulePhone.phone">
+                        <i slot="prefix" class="account"></i>
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="验证码" prop="code">
+                    <el-input type="text" v-model="rulePhone.code">
+                        <i slot="prefix" class="el-input__icon el-icon-search"></i>
+                    </el-input>
+                </el-form-item>
+
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+                    <el-button @click="resetForm('ruleForm')">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
     </div>
 </template>
 
@@ -29,18 +51,33 @@ export default {
         var checkName = (rule, value, callback) => {
             if (value === "") {
                 callback(new Error("请输入用户名"));
-            } else if (!isvalidUsername(value)) {
+            } /*else if (!isvalidUsername(value)) {
                 callback(
                     new Error(
                         "用户名要求数字、字母、下划线的组合 数字和字母必须存在 长度为4-15个字符"
                     )
                 );
-            }
+            }*/
+            callback();
+        };
+        var checkNa = (rule, value, callback) => {
+
+
+            if (value === "") {
+                callback(new Error("请输入phone"));
+            } /*else if (!isvalidUsername(value)) {
+                callback(
+                    new Error(
+                        "用户名要求数字、字母、下划线的组合 数字和字母必须存在 长度为4-15个字符"
+                    )
+                );
+            }*/
+            callback();
         };
         var validatePass = (rule, value, callback) => {
             if (value === "") {
                 callback(new Error("请输入密码"));
-            } else if (!isvalidPass(value)) {
+            } /*else if (!isvalidPass(value)) {
                 callback(
                     new Error("密码以字母开头 长度在8~18之间 只能包含字母、数字和下划线")
                 );
@@ -49,9 +86,11 @@ export default {
                     this.$refs.ruleForm.validateField("pass");
                 }
                 callback();
-            }
+            }*/
+            callback();
         };
         return {
+            isShow: true,
             ruleForm: {
                 pass: '',
                 name: ''
@@ -63,19 +102,48 @@ export default {
                 name: [
                     { validator: checkName, trigger: 'blur' }
                 ]
+            },
+            rulePhone: {
+                phone: '',
+                code: ''
+            },
+            rulesPhone: {
+                code: [
+                    { validator: validatePass, trigger: 'blur' }
+                ],
+                phone: [
+                    { validator: checkNa, trigger: 'blur' }
+                ]
             }
         };
     },
     methods: {
+        tabFrom() {
+            this.isShow = !this.isShow;
+        },
         submitForm(formName) {
-            this.$refs[formName].validate((valid) => {
-                if (valid) {
-                    alert('submit!');
-                } else {
-                    console.log('error submit!!');
-                    return false;
-                }
-            });
+            if (this.isShow) {
+                this.$refs['ruleForm'].validate((valid) => {
+                    if (valid) {
+                        alert('ruleForm!');
+                    } else {
+                        console.log('error ruleForm!!');
+                        return false;
+                    }
+                });
+            } else {
+                this.$refs['rulePhone'].validate((valid) => {
+                    if (valid) {
+                        alert('rulePhone!');
+                    } else {
+                        console.log('error rulePhone!!');
+                        return false;
+                    }
+                });
+            }
+
+
+
         },
         resetForm(formName) {
             this.$refs[formName].resetFields();
